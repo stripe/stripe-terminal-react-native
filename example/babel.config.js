0f161e640c80beece72b13474cbb1ed9aabf54d3
jdivock-stripe <jdivock@stripe.com>
2022-02-01 10:35:51 -0800

    use .env for server address in example app (#51)
    android emu specific url

--- example/babel.config.js
+++ example/babel.config.js
@@ -4,6 +4,7 @@ const pak = require('../package.json');
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
+    ['module:react-native-dotenv'],
     [
       'module-resolver',
       {
--- example/src/Config.ts
+++ example/src/Config.ts
@@ -1,1 +1,6 @@
-export const API_URL = 'https://stripe-backend-danwaters.herokuapp.com';
+// @ts-ignore
+import { API_URL as defaultURL, API_URL_ANDROID as androidURL } from '@env';
+import { Platform } from 'react-native';
+
+export const API_URL: string =
+  Platform.OS === 'android' && androidURL ? androidURL : defaultURL;
