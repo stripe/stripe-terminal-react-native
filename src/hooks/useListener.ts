5987f5295ae47915fbd7f4c43ea31559a92510dc
Arek Kubaczkowski <arek.kubaczkowski@gmail.com>
2022-02-04 10:00:54 +0100

    fix: deprecated event listener console warning

--- src/hooks/useListener.ts
+++ src/hooks/useListener.ts
@@ -7,10 +7,10 @@ const eventEmitter = new NativeEventEmitter(
 
 export function useListener(name: string, callback: (...args: any[]) => any) {
   useEffect(() => {
-    eventEmitter.addListener(name, callback);
+    const listener = eventEmitter.addListener(name, callback);
 
     return () => {
-      eventEmitter.removeListener(name, callback);
+      listener.remove();
     };
   }, [name, callback]);
 }
