a7317a8fcd3693406bf064ec86dacd21276e628f
David Henry <dhenry@stripe.com>
2022-03-07 12:47:01 -0500

    ignoring RCTBridge required dispatch_sync to load RNGestureHandlerModule. This may lead to deadlocks

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -32,6 +32,7 @@ import { LogBox } from 'react-native';
 
 LogBox.ignoreLogs([
   'Non-serializable values were found in the navigation state',
+  'RCTBridge required dispatch_sync to load RNGestureHandlerModule. This may lead to deadlocks',
 ]);
 
 const Stack = createStackNavigator();
