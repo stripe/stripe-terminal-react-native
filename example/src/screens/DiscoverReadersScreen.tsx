ec46d86b7182a4bfe0011b289a4813c6e74d12c1
jdivock-stripe <jdivock@stripe.com>
2022-03-14 14:16:26 -0700

    connect to last location if none is provided (#152)
    * connect to last location if none is selected
    
    * connect with last location
    
    * fix tsc

--- example/src/screens/DiscoverReadersScreen.tsx
+++ example/src/screens/DiscoverReadersScreen.tsx
@@ -165,7 +165,7 @@ export default function DiscoverReadersScreen() {
 
     const { reader: connectedReader, error } = await connectBluetoothReader({
       reader,
-      locationId: selectedLocation?.id,
+      locationId: selectedLocation?.id || reader?.location?.id,
     });
 
     if (error) {
