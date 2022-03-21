026e92bf99b0dbeeaf4c274860a065e7afe40149
Arek Kubaczkowski <arek.kubaczkowski@gmail.com>
2022-03-21 16:37:11 +0100

    fix: duplicated event listeners

--- src/components/StripeTerminalContext.ts
+++ src/components/StripeTerminalContext.ts
@@ -1,20 +1,29 @@
 import { createContext } from 'react';
-import type { Reader, InitParams, InitializeResultType } from '../types';
+import type { EventEmitter } from 'react-native';
+import type {
+  Reader,
+  InitParams,
+  InitializeResultType,
+  UserCallbacks,
+} from '../types';
 
 type ContextType = {
   loading: boolean;
   isInitialized: boolean;
   connectedReader?: Reader.Type | null;
   discoveredReaders: Reader.Type[];
+  emitter?: EventEmitter;
   setLoading(value: boolean): void;
   setIsInitialized(value: boolean): void;
   setConnectedReader(value: Reader.Type | null): void;
   setDiscoveredReaders(value: Reader.Type[]): void;
   initialize?(params: InitParams): Promise<InitializeResultType>;
   log(code: string, message?: any): void;
+  setUserCallbacks(callbacks: UserCallbacks): void;
 };
 
 export const StripeTerminalContext = createContext<ContextType>({
+  emitter: undefined,
   discoveredReaders: [],
   loading: false,
   isInitialized: false,
@@ -24,4 +33,5 @@ export const StripeTerminalContext = createContext<ContextType>({
   setConnectedReader: () => {},
   initialize: undefined,
   setDiscoveredReaders: () => {},
+  setUserCallbacks: () => {},
 });
