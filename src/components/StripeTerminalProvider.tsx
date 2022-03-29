51090be5e59bb4011daba69f51fe729d22143aa4
Jason Divock <jdivock@stripe.com>
2022-03-29 13:19:28 -0600

    memoize context

--- src/components/StripeTerminalProvider.tsx
+++ src/components/StripeTerminalProvider.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useRef, useState } from 'react';
+import React, { useCallback, useRef, useState, useMemo } from 'react';
 import type {
   Reader,
   InitParams,
@@ -237,9 +237,9 @@ export function StripeTerminalProvider({
   useListener(CHANGE_PAYMENT_STATUS, didChangePaymentStatus);
   useListener(CHANGE_CONNECTION_STATUS, didChangeConnectionStatus);
 
-  const setUserCallbacks = (callbacks: UserCallbacks) => {
+  const setUserCallbacks = useCallback((callbacks: UserCallbacks) => {
     userCallbacks.current = callbacks;
-  };
+  }, []);
 
   const _initialize = useCallback(
     async (params: InitParams) => {
@@ -265,29 +265,38 @@ export function StripeTerminalProvider({
     [setLoading, setConnectedReader, setIsInitialized, log]
   );
 
+  const value = useMemo(
+    () => ({
+      loading,
+      isInitialized,
+      connectedReader,
+      discoveredReaders,
+      setIsInitialized,
+      setLoading,
+      setConnectedReader,
+      setDiscoveredReaders,
+      log,
+      initialize: _initialize,
+      setUserCallbacks,
+      emitter,
+    }),
+    [
+      _initialize,
+      loading,
+      isInitialized,
+      connectedReader,
+      discoveredReaders,
+      setIsInitialized,
+      setLoading,
+      setConnectedReader,
+      setDiscoveredReaders,
+      log,
+      setUserCallbacks,
+    ]
+  );
+
   return (
-    <StripeTerminalContext.Provider
-      value={{
-        loading,
-        isInitialized,
-        connectedReader,
-        discoveredReaders,
-        setIsInitialized,
-        setLoading: (value) => {
-          setLoading(value);
-        },
-        setConnectedReader: (value) => {
-          setConnectedReader(value);
-        },
-        setDiscoveredReaders: (values) => {
-          setDiscoveredReaders(values);
-        },
-        log,
-        initialize: _initialize,
-        setUserCallbacks,
-        emitter,
-      }}
-    >
+    <StripeTerminalContext.Provider value={value}>
       {children}
     </StripeTerminalContext.Provider>
   );
