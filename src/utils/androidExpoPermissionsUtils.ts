8e9c68d46b7e76d909a6d2f1cf39eb3fbe39aef3
Jason Divock <jdivock@stripe.com>
2022-03-30 18:02:12 -0600

    refactor permissions utils

--- src/utils/androidExpoPermissionsUtils.ts
+++ src/utils/androidExpoPermissionsUtils.ts
@@ -18,36 +18,52 @@ type PermissionsProps = {
 const isAndroid12orHigher = () =>
   Platform.OS === 'android' && Platform.Version >= 31;
 
+type Error = { error: Record<string, string> | null };
+
 export async function requestNeededExpoAndroidPermissions({
   accessFineLocation = defaultFineLocationParams,
-}: PermissionsProps | undefined = {}): Promise<boolean> {
-  let hasGrantedLocationPermissions = false;
-  let hasGrantedBluetoothPermissions = false;
-
+}: PermissionsProps | undefined = {}): Promise<Error> {
   const grantedFineLocation = await PermissionsAndroid.request(
     PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
     accessFineLocation || defaultFineLocationParams
   );
 
-  if (hasGrantedPermission(grantedFineLocation)) {
-    hasGrantedLocationPermissions = true;
-
-    if (isAndroid12orHigher()) {
-      // otherwise within expo we have to make use of react-native-permissions
-      const grantedBT = await request(PERMISSIONS.ANDROID.BLUETOOTH_CONNECT);
-      const grantedBTScan = await request(PERMISSIONS.ANDROID.BLUETOOTH_SCAN);
-
-      if (
-        hasGrantedPermission(grantedBT) &&
-        hasGrantedPermission(grantedBTScan)
-      ) {
-        hasGrantedBluetoothPermissions = true;
-      }
-    } else {
-      hasGrantedBluetoothPermissions = true;
-    }
+  if (!hasGrantedPermission(grantedFineLocation)) {
+    return {
+      error: {
+        [PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION]:
+          grantedFineLocation,
+      },
+    };
   }
-  return hasGrantedBluetoothPermissions && hasGrantedLocationPermissions;
+
+  if (!isAndroid12orHigher()) {
+    // we're done, we don't need to ask for BT perms
+    return { error: null };
+  }
+
+  // otherwise within expo we have to make use of react-native-permissions
+  const grantedBT = await request(PERMISSIONS.ANDROID.BLUETOOTH_CONNECT);
+
+  if (!hasGrantedPermission(grantedBT)) {
+    return {
+      error: {
+        [PERMISSIONS.ANDROID.BLUETOOTH_CONNECT]: grantedBT,
+      },
+    };
+  }
+
+  const grantedBTScan = await request(PERMISSIONS.ANDROID.BLUETOOTH_SCAN);
+
+  if (!hasGrantedPermission(grantedBTScan)) {
+    return {
+      error: {
+        [PERMISSIONS.ANDROID.BLUETOOTH_SCAN]: grantedBTScan,
+      },
+    };
+  }
+
+  return { error: null };
 }
 
 const hasGrantedPermission = (status: string) => {
--- src/utils/androidPermissionsUtils.ts
+++ src/utils/androidPermissionsUtils.ts
@@ -14,44 +14,61 @@ type PermissionsProps = {
   };
 };
 
+type Error = { error: Record<string, string> | null };
+
 const isAndroid12orHigher = () =>
   Platform.OS === 'android' && Platform.Version >= 31;
 
 export async function requestNeededAndroidPermissions({
   accessFineLocation = defaultFineLocationParams,
-}: PermissionsProps | undefined = {}): Promise<boolean> {
-  let hasGrantedLocationPermissions = false;
-  let hasGrantedBluetoothPermissions = false;
-
+}: PermissionsProps | undefined = {}): Promise<Error> {
   const grantedFineLocation = await PermissionsAndroid.request(
     PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
     accessFineLocation || defaultFineLocationParams
   );
 
-  if (hasGrantedPermission(grantedFineLocation)) {
-    hasGrantedLocationPermissions = true;
-
-    if (isAndroid12orHigher()) {
-      const grantedBT = await PermissionsAndroid.request(
-        // BLUETOOTH_CONNECT doesn't support customization
-        PermissionsAndroid.PERMISSIONS.BLUETOOTH_CONNECT
-      );
-
-      const grantedBTScan = await PermissionsAndroid.request(
-        // BLUETOOTH_SCAN doesn't support customization
-        PermissionsAndroid.PERMISSIONS.BLUETOOTH_SCAN
-      );
-      if (
-        hasGrantedPermission(grantedBT) &&
-        hasGrantedPermission(grantedBTScan)
-      ) {
-        hasGrantedBluetoothPermissions = true;
-      }
-    } else {
-      hasGrantedBluetoothPermissions = true;
-    }
+  if (!hasGrantedPermission(grantedFineLocation)) {
+    return {
+      error: {
+        [PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION]:
+          grantedFineLocation,
+      },
+    };
   }
-  return hasGrantedBluetoothPermissions && hasGrantedLocationPermissions;
+
+  if (!isAndroid12orHigher()) {
+    // we're done, we don't need to ask for BT perms
+    return { error: null };
+  }
+
+  // otherwise within expo we have to make use of react-native-permissions
+  const grantedBT = await PermissionsAndroid.request(
+    // BLUETOOTH_CONNECT doesn't support customization
+    PermissionsAndroid.PERMISSIONS.BLUETOOTH_CONNECT
+  );
+
+  if (!hasGrantedPermission(grantedBT)) {
+    return {
+      error: {
+        [PermissionsAndroid.PERMISSIONS.BLUETOOTH_CONNECT]: grantedBT,
+      },
+    };
+  }
+
+  const grantedBTScan = await PermissionsAndroid.request(
+    // BLUETOOTH_SCAN doesn't support customization
+    PermissionsAndroid.PERMISSIONS.BLUETOOTH_SCAN
+  );
+
+  if (!hasGrantedPermission(grantedBTScan)) {
+    return {
+      error: {
+        [PermissionsAndroid.PERMISSIONS.BLUETOOTH_SCAN]: grantedBTScan,
+      },
+    };
+  }
+
+  return { error: null };
 }
 
 const hasGrantedPermission = (status: string) => {
