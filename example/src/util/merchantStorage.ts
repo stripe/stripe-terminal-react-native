693d17f78c0ed77018b612c9e80b4294e82c1cf1
Jason Divock <jdivock@stripe.com>
2022-04-08 10:17:53 -0600

    set CI accounts in storage shim

--- example/src/util/merchantStorage.ts
+++ example/src/util/merchantStorage.ts
@@ -1,3 +1,9 @@
+import {
+  API_KEY,
+  CA_API_KEY,
+  // @ts-ignore
+} from '@env';
+
 import type { IShortAccount } from '../types';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
@@ -6,18 +12,32 @@ const ACCOUNTS_KEY = '@rn_example_accounts';
 
 export const getStoredAccounts = async (): Promise<Array<IShortAccount>> => {
   const jsonValue = await AsyncStorage.getItem(ACCOUNTS_KEY);
-  const parsedValue = jsonValue ? JSON.parse(jsonValue) : [];
-  return parsedValue;
+  const accts = jsonValue ? JSON.parse(jsonValue) : [];
+
+  if (API_KEY) {
+    accts.push({
+      id: 'acct_1234',
+      name: 'CI US TEST ACCT',
+      secretKey: API_KEY,
+    });
+  }
+
+  if (CA_API_KEY) {
+    accts.push({
+      id: 'acct_5555',
+      name: 'CI CA TEST ACCT',
+      secretKey: CA_API_KEY,
+    });
+  }
+
+  return accts;
 };
 
-export const setStoredAccounts = async (accounts: Array<IShortAccount>) => {
+export const setStoredAccounts = async (accounts: Array<IShortAccount>) =>
   await AsyncStorage.setItem(ACCOUNTS_KEY, JSON.stringify(accounts));
-};
 
-export const getSelectedAccount = async (): Promise<string | null> => {
-  return await AsyncStorage.getItem(SELECTED_ACCOUNT_KEY);
-};
+export const getSelectedAccount = async (): Promise<string | null> =>
+  (await AsyncStorage.getItem(SELECTED_ACCOUNT_KEY)) || API_KEY;
 
-export const setSelectedAccount = async (accountKey: string) => {
+export const setSelectedAccount = async (accountKey: string) =>
   await AsyncStorage.setItem(SELECTED_ACCOUNT_KEY, accountKey);
-};
