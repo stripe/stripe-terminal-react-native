ad7894c7a53307bd5d0c64bbc3d9d25c866b20ca
Jason Divock <jdivock@stripe.com>
2022-04-20 15:55:40 -0600

    solve ios another day

--- dev-app/scripts/fixRN.sh
+++ dev-app/scripts/fixRN.sh
@@ -2,11 +2,21 @@
 
 cd "$(dirname "$0")/../.."
 
-rm node_modules/react-native/scripts/find-node.sh
-touch node_modules/react-native/scripts/find-node.sh
+BASE_FIND=./node_modules/react-native/scripts/find-node.sh
+DEV_FIND=./dev-app/node_modules/react-native/scripts/find-node.sh
+EXAMPLE_FIND=./example-app/node_modules/react-native/scripts/find-node.sh
 
-rm ./example-app/node_modules/react-native/scripts/find-node.sh
-touch ./example-app/node_modules/react-native/scripts/find-node.sh
+if [[ -f "$BASE_FIND" ]]; then
+  rm $BASE_FIND
+  touch $BASE_FIND
+fi
 
-rm ./dev-app/node_modules/react-native/scripts/find-node.sh
-touch ./dev-app/node_modules/react-native/scripts/find-node.sh
+if [[ -f "$EXAMPLE_FIND" ]]; then
+  rm $EXAMPLE_FIND
+  touch $EXAMPLE_FIND
+fi
+
+if [[ -f "$DEV_FIND" ]]; then
+  rm $DEV_FIND
+  touch $DEV_FIND
+fi
