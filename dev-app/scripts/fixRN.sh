0f8dd970e467e5816445757ae55859d984c16b5d
Jason Divock <jdivock@stripe.com>
2022-04-20 15:28:42 -0600

    better fix RN script

--- dev-app/scripts/fixRN.sh
+++ dev-app/scripts/fixRN.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
-cd "$(dirname "$0")/.."
+cd "$(dirname "$0")/../.."
 
 rm node_modules/react-native/scripts/find-node.sh
 touch node_modules/react-native/scripts/find-node.sh
+
+rm ./example-app/node_modules/react-native/scripts/find-node.sh
+touch ./example-app/node_modules/react-native/scripts/find-node.sh
+
+rm ./dev-app/node_modules/react-native/scripts/find-node.sh
+touch ./dev-app/node_modules/react-native/scripts/find-node.sh
