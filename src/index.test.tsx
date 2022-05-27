5d4b492c6e1fc293c5724d558d496edafc241b4a
jdivock-stripe <jdivock@stripe.com>
2022-05-27 09:57:05 -0700

    Sets up initial jest infra and mocks along with a basic export snapshot (#331)
    * js unit infra
    
    * fix lint
    
    * store coverage

---
+++ src/index.test.tsx
@@ -0,0 +1,13 @@
+import * as StripeTerminal from './index';
+
+jest.mock(
+  '../node_modules/react-native/Libraries/EventEmitter/NativeEventEmitter'
+);
+
+describe('index.ts', () => {
+  describe('Public API snapshot', () => {
+    it('ensure there are no unexpected changes to our public exports', () => {
+      expect(StripeTerminal).toMatchSnapshot();
+    });
+  });
+});
