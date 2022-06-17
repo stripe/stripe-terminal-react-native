04c00c7989c7ecd69b2ec077ed5bc86217a86903
Arek Kubaczkowski <arek.kubaczkowski@gmail.com>
2022-06-17 11:22:02 +0200

    prevent calling discoverReaders if not completed

--- android/src/main/java/com/stripeterminalreactnative/StripeTerminalReactNativeModule.kt
+++ android/src/main/java/com/stripeterminalreactnative/StripeTerminalReactNativeModule.kt
@@ -150,6 +150,10 @@ class StripeTerminalReactNativeModule(reactContext: ReactApplicationContext) :
 
         val listener = RNDiscoveryListener(context) { discoveredReadersList = it }
 
+        throwIfBusy(discoverCancelable) {
+            "Could not execute discoverReaders because the SDK is busy with another command: discoverReaders."
+        }
+
         discoverCancelable = terminal.discoverReaders(
             DiscoveryConfiguration(0, discoveryMethod, getBoolean(params, "simulated")),
             listener,
@@ -160,7 +164,9 @@ class StripeTerminalReactNativeModule(reactContext: ReactApplicationContext) :
     @ReactMethod
     @Suppress("unused")
     fun cancelDiscovering(promise: Promise) {
-        cancelOperation(promise, discoverCancelable, "discoverReaders")
+        cancelOperation(promise, discoverCancelable, "discoverReaders") {
+            discoverCancelable = null
+        }
     }
 
     private fun connectReader(
@@ -561,10 +567,12 @@ class StripeTerminalReactNativeModule(reactContext: ReactApplicationContext) :
         promise: Promise,
         cancelable: Cancelable?,
         operationName: String,
+        block: (() -> Unit)? = null
     ) = withExceptionResolver(promise) {
         val toCancel = requireCancelable(cancelable) {
             "$operationName could not be canceled because it has already been canceled or has completed."
         }
         toCancel.cancel(NoOpCallback(promise))
+        block?.invoke()
     }
 }
--- ios/StripeTerminalReactNative.swift
+++ ios/StripeTerminalReactNative.swift
@@ -181,6 +181,11 @@ class StripeTerminalReactNative: RCTEventEmitter, DiscoveryDelegate, BluetoothRe
             discoveryMethod: Mappers.mapToDiscoveryMethod(discoveryMethod),
             simulated: simulated ?? false
         )
+        
+        guard discoverCancelable == nil else {
+            resolve(Errors.createError(code: ErrorCode.busy, message: "Could not execute discoverReaders because the SDK is busy with another command: discoverReaders."))
+            return
+        }
 
         self.discoverCancelable = Terminal.shared.discoverReaders(config, delegate: self) { error in
             if let error = error as NSError? {
