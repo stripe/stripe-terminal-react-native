4b53c0e072f8c9b3b9b738932c3304054d965605
Ian.Lin <ian.lin@bbpos.com>
2023-07-20 10:13:58 +0800

    update react-native to 0.72.1； update dev-app；

--- src/hooks/__tests__/useStripeTerminal.test.tsx
+++ src/hooks/__tests__/useStripeTerminal.test.tsx
@@ -216,7 +216,7 @@ function spyAllFunctions({ returnWith = null }: { returnWith?: any } = {}) {
 
 const createContextWrapper =
   (providerProps: any): React.FC =>
-  ({ children }) =>
+    ({ }) =>
     (
       <StripeTerminalContext.Provider
         value={{
@@ -234,7 +234,7 @@ const createContextWrapper =
           ...providerProps,
         }}
       >
-        {children}
+        { }
       </StripeTerminalContext.Provider>
     );
 
