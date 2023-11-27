8bdbf7d80287accab317da40b46617c976853bb9
Nazli Yurdakul <nazli@stripe.com>
2023-11-27 13:02:31 -0500

    chr feedback

--- dev-app/android/app/src/release/java/com/dev/app/stripeterminalreactnative/ReactNativeFlipper.java
+++ dev-app/android/app/src/release/java/com/dev/app/stripeterminalreactnative/ReactNativeFlipper.java
@@ -1,9 +1,3 @@
-/**
- * Copyright (c) Meta Platforms, Inc. and affiliates.
- *
- * <p>This source code is licensed under the MIT license found in the LICENSE file in the root
- * directory of this source tree.
- */
 package com.dev.app.stripeterminalreactnative;
 import android.content.Context;
 import com.facebook.react.ReactInstanceManager;
