fb6c18c5ec6c6c3f0afbfdf714e7cdec12faad2b
Ian Lin <ian.lin@bbpos.com>
2024-01-30 21:40:11 +0800

    modify timeout to 32000

--- e2e/utils.js
+++ e2e/utils.js
@@ -9,7 +9,7 @@ export const navigateTo = async (buttonText) => {
 export const connectReader = async (name = 'chipper2X') => {
   await waitFor(element(by.text(`SimulatorID - ${name}`)))
     .toBeVisible()
-    .withTimeout(16000);
+    .withTimeout(32000);
   const button = element(by.text(`SimulatorID - ${name}`));
   await button.tap();
 };
