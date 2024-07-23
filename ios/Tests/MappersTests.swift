3f01bcec25e486817a12194536238642e9b3f1cc
Brian Cooke <62762265+bric-stripe@users.noreply.github.com>
2024-07-22 17:05:11 -0700

    Wire up ability to write iOS unit tests (#761)
    * rm missing references from the xcproj
    
    not entirely sure what these were from but makes it a bit awkward to work in so proposing to rm assuming CI passes
    
    * Stubbed runnable test
    
    took me a bit but made sense of how stripe-react-native is doing things and followed along. This sets up a mapperstests. The one test can be run from command line with
    
    xcodebuild test -workspace dev-app/ios/StripeTerminalReactNativeDevApp.xcworkspace -destination 'platform=iOS Simulator,name=iPhone 15' -scheme UnitTests
    
    but its not a worthwhile test but should unblock folks to add more.
    
    * Attempt to wire up to bitrise

---
+++ ios/Tests/MappersTests.swift
@@ -0,0 +1,22 @@
+import XCTest
+
+@testable import stripe_terminal_react_native
+import StripeTerminal
+
+final class MappersTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testLocationStatusMapper() {
+        // bric note: Just an example, this isn't really a useful test since this mapper just maps from
+        // and to constants
+        XCTAssertEqual(Mappers.mapFromLocationStatus(.notSet), "notSet")
+    }
+
+}
