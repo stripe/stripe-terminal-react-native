52607692fc610fe97f7bccfded3669a2e18f85c5
Brian Cooke <bric@stripe.com>
2024-07-26 13:43:35 -0700

    Example of using a mirror protocol for testing allowing a mock struct to be used in the tests

--- ios/Tests/MappersTests.swift
+++ ios/Tests/MappersTests.swift
@@ -20,13 +20,40 @@ final class MappersTests: XCTestCase {
     }
 
     func testCollectInputsReturnsMapper() {
-        let output: NSDictionary = Mappers.mapFromCollectInputsResults([])
+        let textResult = TestableTextResult(skipped: false, text: "Written text from the reader", toggles: [
+            ToggleResult.enabled.rawValue as NSNumber,
+            ToggleResult.skipped.rawValue as NSNumber,
+        ])
+        let output: NSDictionary = Mappers.mapFromCollectInputsResults([
+            textResult
+        ])
         XCTAssertNotNil(output.object(forKey: "collectInputResults"))
         XCTAssertTrue(output["collectInputResults"] is [NSDictionary])
+
         let results: [NSDictionary] = output["collectInputResults"] as! [NSDictionary]
-        for result in results {
-            XCTAssertNotNil(result.object(forKey: "skipped"))
-            XCTAssertNotNil(result.object(forKey: "toggles"))
+
+        XCTAssertEqual(results.count, 1)
+        guard let result = results.first else {
+            XCTFail("CollectInput result should have had a result")
+            return
         }
+        guard
+            let skipped = result["skipped"] as? Bool,
+            let toggles = result["toggles"] as? [String],
+            let text = result["text"] as? String else {
+            XCTFail("CollectInput result should have had text, skipped and toggles")
+            return
+        }
+        XCTAssertFalse(skipped)
+        XCTAssertEqual(toggles.count, 2)
+        XCTAssertEqual(toggles[0], "enabled")
+        XCTAssertEqual(toggles[1], "skipped")
+        XCTAssertEqual(text, "Written text from the reader")
     }
+}
+
+struct TestableTextResult : stripe_terminal_react_native.TextResult {
+    var skipped: Bool
+    var text: String?
+    var toggles: [NSNumber]
 }
