da2bbaef05dce9346b5bee5d283f2f7dbe9abaca
eric.lin <eric.lin@bbpos.com>
2024-07-29 15:13:13 +0800

    Change customerId to customer

--- src/types/index.ts
+++ src/types/index.ts
@@ -221,7 +221,7 @@ export type CollectSetupIntentPaymentMethodParams = {
 };
 
 export type CreateSetupIntentParams = {
-  customerId?: string;
+  customer?: string;
 };
 
 export type PaymentIntentResultType =
