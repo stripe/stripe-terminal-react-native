d3e3c50111cd85bfad01132fb627938683d40043
Ahmed Eid <a.eid@yandex.com>
2024-08-11 22:13:26 +0300

    Update withStripeTerminal.ts

--- src/plugin/withStripeTerminal.ts
+++ src/plugin/withStripeTerminal.ts
@@ -139,9 +139,11 @@ const withStripeTerminalIos: ConfigPlugin<StripeTerminalPluginProps> = (
   props
 ) => {
   return withInfoPlist(expoConfig, (config) => {
-
     if (props.bluetoothBackgroundMode) {
-      (config.modResults.UIBackgroundModes ?? []).push("bluetooth-central");
+      if(!Array.isArray(config.modResults.UIBackgroundModes)) {
+        config.modResults.UIBackgroundModes = [];
+      }
+      config.modResults.UIBackgroundModes.push( 'bluetooth-central' )
     }
     
     config.modResults.NSLocationWhenInUseUsageDescription =
