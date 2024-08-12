7581031ea39a307b8c1b2ab6f37acb39220f4107
Ahmed Eid <a.eid@yandex.com>
2024-08-12 19:30:23 +0300

    append to UIBackgroundModes instead of overriding it. (#781)
    * append to UIBackgroundModes instead of overriding it.
    
    * Update withStripeTerminal.ts
    
    * Update withStripeTerminal.ts
    
    * fix format / lint.
    
    * change implementation.

--- src/plugin/withStripeTerminal.ts
+++ src/plugin/withStripeTerminal.ts
@@ -140,7 +140,10 @@ const withStripeTerminalIos: ConfigPlugin<StripeTerminalPluginProps> = (
 ) => {
   return withInfoPlist(expoConfig, (config) => {
     if (props.bluetoothBackgroundMode) {
-      config.modResults.UIBackgroundModes = ['bluetooth-central'];
+      config.modResults.UIBackgroundModes = [
+        ...(config.modResults.UIBackgroundModes ?? []),
+        'bluetooth-central',
+      ];
     }
 
     config.modResults.NSLocationWhenInUseUsageDescription =
