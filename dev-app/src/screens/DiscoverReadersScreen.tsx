4bcc79dc973ce5048b449b737bbe98d6943711b4
tim-lin-bbpos <tim.lin@bbpos.com>
2024-09-21 02:28:55 +0800

    Add timeout in discover reader (#818)

--- dev-app/src/screens/DiscoverReadersScreen.tsx
+++ dev-app/src/screens/DiscoverReadersScreen.tsx
@@ -51,7 +51,8 @@ export default function DiscoverReadersScreen() {
     autoReconnectOnUnexpectedDisconnect,
     setAutoReconnectOnUnexpectedDisconnect,
   } = useContext(AppContext);
-  const { simulated, discoveryMethod, setPendingUpdateInfo } = params;
+  const { simulated, discoveryMethod, discoveryTimeout, setPendingUpdateInfo } =
+    params;
 
   const {
     cancelDiscovering,
@@ -159,7 +160,7 @@ export default function DiscoverReadersScreen() {
     const { error: discoverReadersError } = await discoverReaders({
       discoveryMethod,
       simulated,
-      timeout: 0,
+      timeout: discoveryTimeout,
     });
 
     if (discoverReadersError) {
@@ -169,7 +170,13 @@ export default function DiscoverReadersScreen() {
         navigation.goBack();
       }
     }
-  }, [navigation, discoverReaders, discoveryMethod, simulated]);
+  }, [
+    navigation,
+    discoverReaders,
+    discoveryMethod,
+    discoveryTimeout,
+    simulated,
+  ]);
 
   useEffect(() => {
     simulateReaderUpdate('none');
