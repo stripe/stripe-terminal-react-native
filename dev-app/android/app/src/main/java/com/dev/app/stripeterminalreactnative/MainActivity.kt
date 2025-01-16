f89f08e3a7bc6fed69ebcd1953641c77adce1f4d
tim.lin <tim.lin@bbpos.com>
2025-01-16 10:46:57 +0800

    Update deps

--- dev-app/android/app/src/main/java/com/dev/app/stripeterminalreactnative/MainActivity.kt
+++ dev-app/android/app/src/main/java/com/dev/app/stripeterminalreactnative/MainActivity.kt
@@ -11,7 +11,7 @@ class MainActivity : ReactActivity() {
      * Returns the name of the main component registered from JavaScript. This is used to schedule
      * rendering of the component.
      */
-    override fun getMainComponentName(): String = "RnDiffApp"
+    override fun getMainComponentName(): String = "StripeTerminalReactNativeDevApp"
 
     /**
      * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
