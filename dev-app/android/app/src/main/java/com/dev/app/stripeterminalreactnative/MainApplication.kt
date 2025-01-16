fbb23e12f20ca8316f0ca824957ceacb88726a0e
tim.lin <tim.lin@bbpos.com>
2025-01-16 17:11:45 +0800

    Add missing init code

--- dev-app/android/app/src/main/java/com/dev/app/stripeterminalreactnative/MainApplication.kt
+++ dev-app/android/app/src/main/java/com/dev/app/stripeterminalreactnative/MainApplication.kt
@@ -12,6 +12,7 @@ import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.react.soloader.OpenSourceMergedSoMapping
 import com.facebook.soloader.SoLoader
 import com.stripeterminalreactnative.StripeTerminalReactNativePackage
+import com.stripeterminalreactnative.TerminalApplicationDelegate
 
 class MainApplication : Application(), ReactApplication {
 
@@ -36,6 +37,7 @@ class MainApplication : Application(), ReactApplication {
 
     override fun onCreate() {
         super.onCreate()
+        TerminalApplicationDelegate.onCreate(this);
         SoLoader.init(this, OpenSourceMergedSoMapping)
         if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
             // If you opted-in for the New Architecture, we load the native entry point for this app.
