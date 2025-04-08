0049389ee754bcee9f427165b809a2ac6d7cbf53
tim.lin <tim.lin@bbpos.com>
2025-04-08 14:43:46 -0700

    make id required

--- src/types/index.ts
+++ src/types/index.ts
@@ -517,7 +517,7 @@ export interface NumericResult extends ICollectInputsResult {
 export interface ISelectionButton {
   style: SelectionButtonStyle;
   text: string;
-  id?: string;
+  id: string;
 }
 
 export enum SelectionButtonStyle {
