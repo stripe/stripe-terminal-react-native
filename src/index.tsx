af9af24c91fe784ac8cea1f19761b2dddcd4718f
Max Afonov <maxa@stripe.com>
2025-06-20 11:16:00 -0400

    compress

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 export * from './types';
 export * from './StripeTerminalSdk';
-import { default as StripeTerminalSdk } from './StripeTerminalSdk';
-export { StripeTerminalSdk };
+export { default as StripeTerminalSdk } from './StripeTerminalSdk';
 import * as PackageJson from '../package.json';
 
 export const getSdkVersion = () => {
