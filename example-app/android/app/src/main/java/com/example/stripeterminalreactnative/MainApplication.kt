a8e714a757c65635192bf7a260286534fbfb43f7
tim.lin <tim.lin@bbpos.com>
2025-08-07 16:01:28 +0800

    apply change

--- example-app/android/app/src/main/java/com/example/stripeterminalreactnative/MainApplication.kt
+++ example-app/android/app/src/main/java/com/example/stripeterminalreactnative/MainApplication.kt
@@ -42,7 +42,7 @@ class MainApplication : Application(), ReactApplication {
   override fun onCreate() {
     super.onCreate()
     TerminalApplicationDelegate.onCreate(this);
-    SoLoader.init(this, false)
+    SoLoader.init(this, OpenSourceMergedSoMapping)
     if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
       // If you opted-in for the New Architecture, we load the native entry point for this app.
       load()
