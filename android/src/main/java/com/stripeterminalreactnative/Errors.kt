b7a574cba43ad94688d7f9f6693a98401b6014fb
Matt Yang <myang-c@stripe.com>
2025-09-26 17:38:42 +0800

    Remove useless CancellationException test

--- android/src/main/java/com/stripeterminalreactnative/Errors.kt
+++ android/src/main/java/com/stripeterminalreactnative/Errors.kt
@@ -6,7 +6,6 @@ import com.facebook.react.bridge.WritableMap
 import com.stripe.stripeterminal.external.api.ApiError
 import com.stripe.stripeterminal.external.models.TerminalErrorCode
 import com.stripe.stripeterminal.external.models.TerminalException
-import kotlinx.coroutines.CancellationException
 
 internal fun createError(throwable: Throwable): ReadableMap = nativeMapOf { putError(throwable) }
 
