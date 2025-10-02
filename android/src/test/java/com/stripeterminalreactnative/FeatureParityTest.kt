07cd1e652c21cfc7782f92c993f858485f7af270
Tim Lin <tlin-c@stripe.com>
2025-10-02 16:31:25 +0800

    test fail case

--- android/src/test/java/com/stripeterminalreactnative/FeatureParityTest.kt
+++ android/src/test/java/com/stripeterminalreactnative/FeatureParityTest.kt
@@ -14,7 +14,7 @@ class FeatureParityTest {
         "setTerminalListener",
         "setOfflineListener",
         "isInitialized",
-        "getInstance",
+//        "getInstance",
         "connectExternalChannel"
     )
 
