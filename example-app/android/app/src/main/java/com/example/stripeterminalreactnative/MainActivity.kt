47bd2944d971c4e4ca93f6fd9370965f80c2b148
Tim Lin <tlin-c@stripe.com>
2025-10-17 11:08:29 +0800

    test

--- example-app/android/app/src/main/java/com/example/stripeterminalreactnative/MainActivity.kt
+++ example-app/android/app/src/main/java/com/example/stripeterminalreactnative/MainActivity.kt
@@ -20,7 +20,7 @@ class MainActivity : ReactActivity() {
     // @generated begin expo-splashscreen - expo prebuild (DO NOT MODIFY) sync-f3ff59a738c56c9a6119210cb55f0b613eb8b6af
     SplashScreenManager.registerOnActivity(this)
     // @generated end expo-splashscreen
-    super.onCreate(null)
+    super.onCreate(savedInstanceState)
   }
 
   /**
